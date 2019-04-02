3e6b90f44a23ca86041b4635572cbb24ba291a8b
xinhaichen <xinhaichen@uqpay.com>
2019-04-02 16:53:03 +0800

    feat:alien_invasion

---
+++ setting.py
@@ -0,0 +1,46 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+'setting moudle'
+__author__ = 'lazyChen'
+
+class Settings():
+    """ 存储《外星人入侵》的所有设置的类"""
+    
+    def __init__(self):
+        """初始化游戏的设置"""
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        #飞船位置
+        self.ship_limit = 3
+        #子弹设置
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60,60,60
+        self.bullets_allowed = 4
+        #外星人设置
+        self.fleet_drop_speed = 10
+        self.initialize_dynamic_settings()
+        #速度变化量
+        self.speedup_scale = 1.1
+        #外星人点数提高的速度
+        self.score_scale = 1.5
+        
+    def initialize_dynamic_settings(self):
+        '''初始化随游戏进行而变化的设置'''
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 0.6
+        #fleet_direction 为1表示向右，为-1表示向左
+        self.fleet_direction = 1
+        #记分
+        self.alien_points = 10
+    
+    def increase_speed(self):
+        '''提高速度的设置'''
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
